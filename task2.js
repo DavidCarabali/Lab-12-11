3a8e53ba4c145f6c4458c5bb96960cfc42b422ef
DavidCarabali <javierdavidcarabali@gmail.com>
2024-12-11 19:07:02 -0500

    Este el archivo actullizado

--- task2.js
+++ task2.js
@@ -1,5 +1,17 @@
+// Exportamos la clase FriendNames para que pueda ser importada en otros archivos
 export class FriendNames {
-    constructor() {
-  
+
+  // El cosntructor recibe tres parametros: 
+    constructor(name1, name2, name3){
+     
+      //Asigamos el valor de nombre1 al atributo nombre1 del objeto
+      this.name1 = name1;
+      this.name2 = name2;
+      this.name3 = name3;
+
     }
   }
+// Se esta instanciando un objeto (hijo) de tipo Player (papa que es la clase)
+  const friend = new FriendNames("Karim", "Uli", "Georgina");
+
+console.log(friend);
